d5052435d1ab02285ffeae3b21dabdbea1c2ac55
Aryan Sahu <sahuaryan0325@gmail.com>
2026-05-15 11:37:17 +0530

    Module CSS Learned

--- 05-css/src/App.jsx
+++ 05-css/src/App.jsx
@@ -1,7 +1,13 @@
 import React from 'react'
+import Navbar from './Components/Navbar/navbar'
+import Button from './Components/Button/button'
 
 export default function App() {
   return (
+    <div className="Parent">
     <div>App</div>
+    <div><Navbar /></div>
+    <div><Button /></div>
+    </div>
   )
 }
---
+++ 05-css/src/Components/Button/button.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import styles from'./button.module.css'
+
+export default function Button() {
+  return (
+    <div className={styles.btn}>This is a button</div>
+  )
+}

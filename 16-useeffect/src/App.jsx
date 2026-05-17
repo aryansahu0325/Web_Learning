4f19560ea44dd4f4043537408008b19f8a8a66bc
Aryan Sahu <sahuaryan0325@gmail.com>
2026-05-17 17:49:28 +0530

    Using USeEffect

---
+++ 16-useeffect/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { useEffect } from 'react'
+
+const App = () => {
+useEffect(function random(){
+  console.log('use effect is running...');
+})
+
+
+  return (
+    <div>App</div>
+  )
+}
+
+export default App
